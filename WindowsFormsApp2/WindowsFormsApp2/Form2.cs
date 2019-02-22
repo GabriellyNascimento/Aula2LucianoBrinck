5d47c01b67b1ffe2d0a3f55f9bac81e1a591ed87
GabriellyNascimento <nascimentogabrielly80@gmail.com>
2019-02-21 21:18:40 -0300

    Exercicio Aula 2

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void tableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.segundaAulaDataSet);
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'segundaAulaDataSet.Table'. Você pode movê-la ou removê-la conforme necessário.
+            this.tableTableAdapter.Fill(this.segundaAulaDataSet.Table);
+
+        }
+
+        private void botaomais(object sender, EventArgs e)
+        {
+            nomeTextBox.Enabled = true;
+            cidadeTextBox.Enabled = true;
+            telefoneTextBox.Enabled = true;
+        }
+    }
+}
